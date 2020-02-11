eb608df115d7d68144d64c41f97e3013d7ed176d
SeokYoung Shin <aceorb@gmail.com>
2020-02-11 23:41:00 +0900

    Initial commit.

---
+++ Client/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open HelloWorld
+open Grpc.Core
+
+
+type HelloServiceImpl(client : HelloService.HelloServiceClient) =
+        member this.SayHello(greeting : string) =
+            let request = new HelloReq(Greeting = greeting)
+            client.SayHello(request)
+
+
+[<EntryPoint>]
+let main argv =
+    let channel = new Channel("127.0.0.1:5000", ChannelCredentials.Insecure)
+    let client = new HelloServiceImpl(new HelloService.HelloServiceClient(channel))
+    
+    let result = client.SayHello("Hi~")
+    Console.WriteLine(result.Reply)
+    0 // return an integer exit code
---
+++ Server/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.Threading.Tasks
+open Grpc.Core
+open HelloWorld
+
+
+type HelloServiceImpl() =
+    inherit HelloService.HelloServiceBase()
+    
+    override this.SayHello(request : HelloReq, context :ServerCallContext) : Task<HelloRes> =
+        HelloRes(Reply = request.Greeting+" 안녕?") |> Task.FromResult
+
+
+
+[<EntryPoint>]
+let main argv =
+    let port = 5000
+    let server = Server()
+    server.Services.Add(HelloService.BindService(new HelloServiceImpl()))
+    server.Ports.Add(ServerPort("127.0.0.1", port, ServerCredentials.Insecure)) |> ignore
+    server.Start()
+    
+    Console.WriteLine("Press any key to stop the server ...")
+    Console.ReadKey() |> ignore
+    
+    server.ShutdownAsync().Wait()
+    0 // return an integer exit code
