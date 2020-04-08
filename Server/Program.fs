c1ba4a9b59b4c54722c05840e14086d4172caa5f
SeokYoung Shin <aceorb@gmail.com>
2020-04-08 23:42:43 +0900

    Update Program.fs

--- Server/Program.fs
+++ Server/Program.fs
@@ -1,14 +1,14 @@
 ﻿open System
 open System.Threading.Tasks
 open Grpc.Core
-open HelloWorld
+open Protocol.ProtoBuf          // Use the package name in .proto file
 
 
 type HelloServiceImpl() =
     inherit HelloService.HelloServiceBase()
     
-    override this.SayHello(request : HelloReq, context :ServerCallContext) : Task<HelloRes> =
-        HelloRes(Reply = request.Greeting+" 안녕?") |> Task.FromResult
+    override _.SayHello(request : HelloReq, context :ServerCallContext) : Task<HelloRes> =
+        HelloRes(Reply = request.Greeting+" Hello?") |> Task.FromResult
 
 
 
